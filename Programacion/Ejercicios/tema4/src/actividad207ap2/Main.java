27861a58fb5b86b0d6e1fa7448542ecd10c8efed
Juan Gómez Ruiz <jgomrui1410@gmail.com>
2026-01-20 12:46:55 +0100

    Programacion

--- Programacion/Ejercicios/tema4/src/actividad207ap2/Main.java
+++ Programacion/Ejercicios/tema4/src/actividad207ap2/Main.java
@@ -1,11 +1,10 @@
 package actividad207ap2;
 
-import modelo.Vehiculo;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Movimiento automovil = new Movimiento("1231232G", "BMW", 120, null, null);
+		Movimiento automovil = new Movimiento("1231232G", "BMW", 120, "aaa", 0, "Motor", "Tierra");
 		
 		
 		automovil.tipoVehiculo();
--- Programacion/Ejercicios/tema4/src/actividad207ap2/Movimiento.java
+++ Programacion/Ejercicios/tema4/src/actividad207ap2/Movimiento.java
@@ -1,6 +1,6 @@
 package actividad207ap2;
 
-import modelo.Vehiculo;
+import actividad406.Vehiculo;
 
 public class Movimiento extends Vehiculo {
 	
@@ -11,8 +11,8 @@ public class Movimiento extends Vehiculo {
 	
 	// CONSTRUCTOR
 	
-	public Movimiento(String matricula, String marca, int velocidad, String aceleracion, String tipoVehiculo) {
-		super(matricula,marca,velocidad);
+	public Movimiento(String matricula, String marca, int velocidad,String modelo, int anoMatriculacion ,String aceleracion, String tipoVehiculo) {
+		super(matricula,marca,modelo,anoMatriculacion);
 		this.aceleracion = aceleracion;
 		this.tipoVehiculo = tipoVehiculo;
 	}
@@ -21,12 +21,16 @@ public class Movimiento extends Vehiculo {
 		switch(aceleracion) {
 		case "Motor":
 			System.out.println("Este vehiculo va con motor");
+			break;
 		case "Sin Motor":
 			System.out.println("Este vehiculo va sin motor");
+			break;
 		case "Traccion Animal":
 			System.out.println("Este vehiculo va con tracción animal");
+			break;
 		default:
 			System.out.println("No se ha especificado");
+			break;
 		}
 	}
 	
@@ -34,13 +38,29 @@ public class Movimiento extends Vehiculo {
 		switch(tipoVehiculo) {
 		case "Tierra":
 			System.out.println("Este vehículo va por la tierra");
+			break;
 		case "Aire":
-			System.out.println("Este vehiculo va por el aire");
+			comprobacionAire();
+			break;
 		case "Mar":
 			System.out.println("Este vehiculo va por el mar");
+			break;
 		default:
 			System.out.println("No se ha especificado");
 		}
 	}
+	
+	public void comprobacionAire() {
+		if(tipoVehiculo.equals("Aire") || aceleracion.equals("Sin Motor")) {
+			System.out.println("No puedes viajar por aire sin un motor");
+		}else
+			if(tipoVehiculo.equals("Aire") || aceleracion.equals("Traccion Animal")) {
+				System.out.println("No puedes volar por el aire tirado de pajaros");
+			}else
+				System.out.println("Este vehiculo va por el aire");
+	}
+	
+	
+	}
 
-}
+
