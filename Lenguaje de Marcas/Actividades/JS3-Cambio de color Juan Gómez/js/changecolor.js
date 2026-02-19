126bff25a06fdb8d9902a2004a3e38f046068337
Rimantico <jgomrui1410@gmail.com>
2026-02-19 17:48:21 +0100

    Lenguaje de Marcas

--- Lenguaje de Marcas/Actividades/JS3-Cambio de color Juan Gómez/js/changecolor.js
+++ Lenguaje de Marcas/Actividades/JS3-Cambio de color Juan Gómez/js/changecolor.js
@@ -5,61 +5,103 @@ let btnAmarillo= document.getElementById('btnamarillo');
 let btnRosa = document.getElementById('btnrosa');
 let btnReset = document.getElementById('btnreset');
 let box = document.querySelector('.box');
+let btnImagen = document.getElementById('btnimagen')
+
 
 // Poner color boton
 
 function setColorRojo() {
   if(btnRojo.value == "rojo") {
     box.style.background = "#FF0000";
+    editarTexto.style.color="#FF0000"
   }
 }
 
 function setColorVerde() {
   if(btnVerde.value == "verde") {
     box.style.background = "#00FF00";
+      editarTexto.style.color="#00FF00"
   }
 }
 
 function setColorAzul() {
   if(btnAzul.value == "azul") {
     box.style.background = "#0000FF";
+      editarTexto.style.color="#0000FF"
   }
 }
 
 function setColorAmarillo() {
   if(btnAmarillo.value == "amarillo") {
     box.style.background = "#FFFF00";
+    editarTexto.style.color="#FFFF00"
   }
 }
 
 function setColorRosa(){
   if(btnRosa.value == "rosa"){
     box.style.background = "#FFC0CB";
+    editarTexto.style.color="#FFC0CB"
   }
 }
 
 function setColorReset() {
   if(btnReset.value == "reset") {
     box.style.background = "#000000";
+    editarTexto.style.color="#000000"
   }
 }
 
+// Actividad 1
+
+// Cambiar imagen
+
+var imagen =  document.getElementById('imagen')
+
+imagen.style.height="20vh"
+
+
+function cambiarImagen(){
+  if(imagen.src.match("imagen1.jfif")){
+    imagen.src="./images/imagen2.jpg"
+  }else{
+    imagen.src = "./images/imagen1.jfif"
+  }
+}
+
+// Cambiar el titulo de la pagina
+
+let editarTexto = document.getElementById('titulo')
+editarTexto.style.backgroundColor="white"
+editarTexto.style.marginBottom="10px"
+
 // Listeners para los botones
+
 
 btnRojo.addEventListener("mouseover" ,botonEncimaRojo)
 btnRojo.addEventListener("mouseout",botonSalidaRojo)
+btnRojo.addEventListener("mousedown", botonPresionadoRojo)
+btnRojo.addEventListener("mouseup" ,botonDejarRojo)
 
 btnVerde.addEventListener("mouseover", botonEncimaVerde)
 btnVerde.addEventListener("mouseout", botonSalidaVerde)
+btnVerde.addEventListener("mousedown", botonPresionadoVerde)
+btnVerde.addEventListener("mouseup", botonDejarVerde)
 
 btnAzul.addEventListener("mouseover", botonEncimaAzul)
 btnAzul.addEventListener("mouseout" , botonSalidaAzul)
+btnAzul.addEventListener("mousedown", botonPresionadoAzul)
+btnAzul.addEventListener("mouseup" ,botonDejarAzul)
 
 btnAmarillo.addEventListener("mouseover" , botonEncimaAmarillo)
 btnAmarillo.addEventListener("mouseout" , botonSalidaAmarillo)
+btnAmarillo-addEventListener("mousedown", botonPresionadoAmarillo)
+btnAmarillo,addEventListener("mouseup", botonDejarAmarillo)
 
 btnRosa.addEventListener("mouseover", botonEncimaRosa)
 btnRosa.addEventListener("mouseout", botonSalidaRosa)
+btnRosa.addEventListener("mousedown", botonPresionadoRosa)
+btnRosa.addEventListener("mouseup", botonDejarRosa)
 
 btnReset.addEventListener("mouseout", botonEncimaReset)
 btnReset.addEventListener("mouseout", botonSalidaReset)
@@ -113,4 +155,46 @@ function botonSalidaRosa(){
 
 function botonSalidaReset(){
   btnReset.style.transform="scale(1,1)"
+}
+
+// Cambiar el color cuando se quede presionado el boton
+
+function botonPresionadoRojo(){
+  btnRojo.style.backgroundColor="red"
+}
+
+function botonPresionadoVerde(){
+  btnVerde.style.backgroundColor="green"
+}
+
+function botonPresionadoAzul(){
+  btnAzul.style.backgroundColor="blue"
+}
+
+function botonPresionadoAmarillo(){
+  btnAmarillo.style.backgroundColor="yellow"
+}
+
+function botonPresionadoRosa(){
+  btnRosa.style.backgroundColor="pink"
+}
+
+// Cambiar el color original cuando se deje de presionar
+
+function botonDejarRojo(){
+  btnRojo.style.backgroundColor="rgb(191, 219, 29)"
+}
+
+function botonDejarVerde(){
+  btnVerde.style.backgroundColor="rgb(191, 219, 29)"
+}
+
+function botonDejarAzul(){
+  btnAzul.style.backgroundColor="rgb(191, 219, 29)"
+}
+function botonDejarAmarillo(){
+  btnAmarillo.style.backgroundColor="rgb(191, 219, 29)"
+}
+function botonDejarRosa(){
+  btnRosa.style.backgroundColor="rgb(191, 219, 29)"
 }
